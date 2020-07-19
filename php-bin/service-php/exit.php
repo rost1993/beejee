f3abbb089d6452d54950b83a15c54ca350ea3116
rost1993 <rostislav-gashin@yandex.ru>
2020-07-19 17:06:22 +0300

    BeeJee

--- php-bin/service-php/exit.php
+++ php-bin/service-php/exit.php
@@ -3,11 +3,9 @@
 session_name('BeeJee');
 session_start();
 
-$root = 'http://' . $_SERVER['HTTP_HOST'] . '/beejee/index.php';	// Путь куда необходимо редиректить в случае выхода
-
 $_SESSION = array();
 session_unset();
 session_destroy();
-header('location: ' . $root);
+header('location: ../../index.php');
 
 ?>
