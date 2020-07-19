488d1f82ea225808f94edc832ff37f924ec3fb02
rost1993 <rostislav-gashin@yandex.ru>
2020-07-19 14:29:10 +0300

    BeeJee

--- php-bin/elements-design/footer-bottom.php
+++
@@ -1,8 +0,0 @@
-<?php
-	echo "<footer class='navbar navbar-expand-lg navbar-dark bottom-footer-template'>"
-			. "<div class='container-fluid'>"
-				. "<div class='col-12 text-center'>"
-					. "<p class='text-brand-ic'>Веб-ресурс разработан Информационным центром МВД по Республике Коми<br>"
-						. "<a href='http://10.66.10.30:8080' style='color: #ffffff;'>Информационный центр МВД по Республике Коми </a>&#169</p>"
-		. "</div></div></footer>";
-?>
